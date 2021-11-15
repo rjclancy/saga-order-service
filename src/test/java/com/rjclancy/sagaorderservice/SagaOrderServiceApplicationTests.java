b2f2e41d29988efcff51612a2483222309ed3d90
ronanclancy <macuser@Macs-MacBook-Pro.local>
2021-11-15 14:02:33 +0000

    initial-commit

---
+++ src/test/java/com/rjclancy/sagaorderservice/SagaOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjclancy.sagaorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
